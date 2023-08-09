1ac05a6410036d437f6b40f226717e333a9bd671
Sahil Kumar <sahilku601@gmail.com>
2023-08-10 03:40:30 +0530

    - Added Error handling message - A basic loading function

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,44 @@
-import './index.css'
+import './index.css';
 import { useState, useEffect } from 'react';
 import BlogList from './BlogLists';
 
 const Home = () => {
-    const [blog, setblog] = useState([null]);
-    const [ispending,setpending]=useState(true);
-    function handleDelete(id){
-        const newblogs=blog.filter(blog => blog.id !== id);
+    const [error,seterror]=useState();
+    const [blog, setblog] = useState([]);
+    const [ispending, setpending] = useState(true);
+
+    function handleDelete(id) {
+        const newblogs = blog.filter(blog => blog.id !== id);
         setblog(newblogs);
     }
-    useEffect(()=>{
-        console.log('use effect arn')
-        setTimeout(()=>{
+
+    useEffect(() => {
+        setTimeout(() => {
             fetch("http://localhost:4040/blog")
-            .then((res)=>{
-                return res.json();
-            })
-            .then(data=>{
-                setblog(data);
-                setpending(false)
-            });
-        },1000)
-    },[])
-    return ( 
+                .then((res) => {
+                    if (!res.ok) {
+                        throw Error("Couldn't Fetch the Data For that Request");
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setblog(data);
+                    setpending(false);
+                })
+                .catch((err) => {
+                    setpending(false);
+                    seterror(err.message);
+                });
+        }, 1000);
+    }, []);
+
+    return (
         <div className="content">
-            {ispending&&<div>Is Loading</div>}
-            {/* {blog && <BlogList  blogs={blog} title="New Blogs"/>}; */}
+            {error && <div>{error}</div>}
+            {ispending && <div>Is Loading</div>}
+            {blog && <BlogList blogs={blog} title="New Blogs" />}
         </div>
-     )
+    );
 }
- 
+
 export default Home;
