2b09fea66ee3d1ba059ba2a45c317b3a131a4b4f
Sahil Kumar <sahilku601@gmail.com>
2023-08-11 00:58:59 +0530

    -Used a Custom Hook are Used

--- src/Home.js
+++ src/Home.js
@@ -1,37 +1,10 @@
 import './index.css';
 import { useState, useEffect } from 'react';
 import BlogList from './BlogLists';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [error,seterror]=useState();
-    const [blog, setblog] = useState([]);
-    const [ispending, setpending] = useState(true);
-
-    function handleDelete(id) {
-        const newblogs = blog.filter(blog => blog.id !== id);
-        setblog(newblogs);
-    }
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch("http://localhost:4040/blog")
-                .then((res) => {
-                    if (!res.ok) {
-                        throw Error("Couldn't Fetch the Data For that Request");
-                    }
-                    return res.json();
-                })
-                .then(data => {
-                    setblog(data);
-                    setpending(false);
-                })
-                .catch((err) => {
-                    setpending(false);
-                    seterror(err.message);
-                });
-        }, 1000);
-    }, []);
-
+    const{data:blog,ispending,error}=useFetch("http://localhost:4040/blog");
     return (
         <div className="content">
             {error && <div>{error}</div>}
